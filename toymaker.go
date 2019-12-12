9d7bd1b6848751235e296425d6522ae05090619e
KennyTheBard <octav_teculescu@yahoo.com>
2019-12-13 00:49:23 +0200

    Testing the current progress of the program (seg fault)

--- toymaker.go
+++ toymaker.go
@@ -1,2 +1,26 @@
-package toymaker
+package main
 
+import (
+	"image"
+	"fmt"
+	"image/draw"
+
+	freetype "github.com/golang/freetype"
+
+	loader "./loader"
+)
+
+func main() {
+	context := freetype.NewContext()
+	img, _ := loader.LoadImage("images/test.jpg")
+	font, _ := loader.LoadFont("fonts/OldLondon.ttf")
+	pt := freetype.Pt(0, 0)
+
+	context.SetDst(img.(draw.Image))
+	context.SetClip(image.Rect(0, 0, 100, 500))
+	context.SetFont(font)
+	context.SetFontSize(25)
+	context.DrawString("This is a long ass text designed to test text wrapping", pt)
+
+	loader.SaveImageJPEG(img, "output")
+}
